6670572e3967b2531c461f965748ef9784b0ba84
Santhosh Alagiri Muthukumar <santhoshalagiri@gmail.com>
2022-08-08 15:45:33 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC1XtUCuw0MsKSzOwFYUL1FiEPT6_QFnc4",
+    authDomain: "challenge-e6e80.firebaseapp.com",
+    projectId: "challenge-e6e80",
+    storageBucket: "challenge-e6e80.appspot.com",
+    messagingSenderId: "778948626360",
+    appId: "1:778948626360:web:42ec3c68ad1ead2f5d714d",
+    measurementId: "G-40VTN9KDYB"
+  };
